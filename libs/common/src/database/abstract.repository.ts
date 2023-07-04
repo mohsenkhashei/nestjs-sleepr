c957b96e6f1d0919776ecd7f8ac90d0d90c1395e
MohsenKhashei <m.khasheii@gmail.com>
2023-07-04 15:17:02 +0300

    add findOneAndDelete

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -46,4 +46,10 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
     return this.model.find(filterQuery, {}, { lean: true });
   }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return this.model.findOneAndDelete(filterQuery, { lean: true });
+  }
 }
