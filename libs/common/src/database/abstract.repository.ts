f203f91213bca1d2c0e13f2d8da674a5a1427609
MohsenKhashei <m.khasheii@gmail.com>
2023-07-04 15:14:48 +0300

    add find

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -42,4 +42,8 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
 
     return document;
   }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery, {}, { lean: true });
+  }
 }
