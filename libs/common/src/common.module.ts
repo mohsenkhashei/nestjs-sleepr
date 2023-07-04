5c63df10208377bb3e5fc23972f7cb0c600ccc33
MohsenKhashei <m.khasheii@gmail.com>
2023-07-04 11:40:29 +0300

    add config

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   providers: [CommonService],
   exports: [CommonService],
+  imports: [DatabaseModule, ConfigModule],
 })
 export class CommonModule {}
