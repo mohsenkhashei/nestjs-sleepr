c27f4031aa03998f0adc5145a0677e11042de3b6
MohsenKhashei <m.khasheii@gmail.com>
2023-07-05 09:45:42 +0300

    add pino logger as module in common area

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { Logger } from 'nestjs-pino';
 
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -7,6 +7,7 @@ import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
+import { LoggerModule } from '@app/common/logger/logger.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import {
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
