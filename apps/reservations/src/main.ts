38f4542828cc0a213c7d27e563a8642eb49c7b6a
MohsenKhashei <m.khasheii@gmail.com>
2023-07-05 09:59:02 +0300

    adding validation

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,8 +5,10 @@ import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+
   await app.listen(3000);
 }
 bootstrap();
