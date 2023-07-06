0bd267e9696d10930f49b2b37ea5896dd5437b99
MohsenKhashei <m.khasheii@gmail.com>
2023-07-06 21:51:59 +0300

    read config from config file

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -3,12 +3,14 @@ import { ReservationsModule } from './reservations.module';
 import { Logger } from 'nestjs-pino';
 
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
