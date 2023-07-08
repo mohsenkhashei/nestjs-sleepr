f016d665bf707c9c07149cbba78b2636b10a9499
MohsenKhashei <m.khasheii@gmail.com>
2023-07-08 12:37:31 +0300

    add login

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common/logger/logger.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -32,6 +33,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
