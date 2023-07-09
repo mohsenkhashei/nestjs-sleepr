a5d5bc73de8ff5a20768bc16f270fcf2134bda93
MohsenKhashei <m.khasheii@gmail.com>
2023-07-09 13:24:14 +0300

    add user model in comman

--- libs/common/src/models/user.schema.ts
+++ libs/common/src/models/user.schema.ts
@@ -1,12 +1,16 @@
-import { AbstractDocument } from '@app/common/database/abstract.schema';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AbstractDocument } from '../database/abstract.schema';
 
 @Schema({ versionKey: false })
 export class UserDocument extends AbstractDocument {
   @Prop()
   email: string;
+
   @Prop()
   password: string;
+
+  @Prop()
+  roles?: string[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserDocument);
