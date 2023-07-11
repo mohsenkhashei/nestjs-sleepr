276f5c335b57f38bd1e386cf72b4417f01c64fbd
MohsenKhashei <m.khasheii@gmail.com>
2023-07-11 17:10:17 +0300

    add notifications service

---
+++ apps/notifications/src/notifications.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { LoggerModule } from '@app/common/logger/logger.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`./apps/payments/.env`],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+    LoggerModule,
+  ],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
+})
+export class NotificationsModule {}
