63f16b5940d84866c9ddbef8901fe7958fd4ab2e
MohsenKhashei <m.khasheii@gmail.com>
2023-07-13 17:38:27 +0300

    add validtion to env file

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -12,6 +12,10 @@ import { LoggerModule } from '@app/common/logger/logger.module';
       isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
+        GOOGLE_OAUTH_CLIENT_ID: Joi.string().required(),
+        GOOGLE_OAUTH_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_OAUTH_REFRESH_TOKEN: Joi.string().required(),
+        SMTP_USER: Joi.string().required(),
       }),
     }),
     LoggerModule,
